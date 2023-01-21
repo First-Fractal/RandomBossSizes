1ff31c5a6cc6319f3b8bb08261a7d8cca4ba93de
First Fractal <firstfractal512@gmail.com>
2023-01-21 13:36:44 -0600

    made the bosses big

--- RandomBossSizes.cs
+++ RandomBossSizes.cs
@@ -1,3 +1,5 @@
+using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace RandomBossSizes
@@ -5,4 +7,17 @@ namespace RandomBossSizes
 	public class RandomBossSizes : Mod
 	{
 	}
+	
+	public class ModifyNPCS : GlobalNPC
+    {
+
+       public override void PostAI(NPC npc)
+        {
+            if (npc.boss)
+            {
+                npc.scale = 1.9f;
+            }
+            base.PostAI(npc);
+        }
+    }
 }
