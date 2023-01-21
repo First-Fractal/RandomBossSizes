3498f461c6f66897c28872cdc21608d20748e7f1
First Fractal <firstfractal512@gmail.com>
2023-01-21 14:40:10 -0600

    made the bosses body parts also affected by the size

--- RandomBossSizes.cs
+++ RandomBossSizes.cs
@@ -11,11 +11,23 @@ namespace RandomBossSizes
 	public class ModifyNPCS : GlobalNPC
     {
 
+       static int[] BossParts = {NPCID.SlimeSpiked, NPCID.ServantofCthulhu, NPCID.EaterofWorldsHead, NPCID.EaterofWorldsBody, NPCID.EaterofWorldsTail, NPCID.Creeper, NPCID.SkeletronHand, NPCID.SkeletronHead, NPCID.WallofFleshEye, NPCID.TheDestroyer, NPCID.TheDestroyerBody, NPCID.TheDestroyerTail, NPCID.Probe, NPCID.PrimeCannon, NPCID.PrimeLaser, NPCID.PrimeSaw, NPCID.PrimeVice, NPCID.PlanterasHook, NPCID.PlanterasTentacle, NPCID.GolemFistLeft, NPCID.GolemFistRight, NPCID.GolemHead, NPCID.GolemHeadFree, NPCID.CultistTablet, NPCID.CultistBossClone, NPCID.LunarTowerNebula, NPCID.LunarTowerSolar, NPCID.LunarTowerVortex, NPCID.LunarTowerStardust, NPCID.MoonLordCore, NPCID.MoonLordHand, NPCID.MoonLordHead, NPCID.MoonLordFreeEye, NPCID.MoonLordLeechBlob };
        public override void PostAI(NPC npc)
         {
+
             if (npc.boss)
             {
+                
                 npc.scale = 1.9f;
+            } else
+            {
+                foreach (int bosspart in BossParts)
+                {
+                    if (npc.type == bosspart)
+                    {
+                        npc.scale = 1.9f;
+                    }
+                }
             }
             base.PostAI(npc);
         }
