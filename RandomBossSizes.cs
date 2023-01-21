315dc1f37cae1d5c8773b7a82e1717b7fdac4c61
First Fractal <firstfractal512@gmail.com>
2023-01-21 15:16:38 -0600

    made the bosses have random sizes

--- RandomBossSizes.cs
+++ RandomBossSizes.cs
@@ -12,24 +12,23 @@ namespace RandomBossSizes
     {
 
        static int[] BossParts = {NPCID.SlimeSpiked, NPCID.ServantofCthulhu, NPCID.EaterofWorldsHead, NPCID.EaterofWorldsBody, NPCID.EaterofWorldsTail, NPCID.Creeper, NPCID.SkeletronHand, NPCID.SkeletronHead, NPCID.WallofFleshEye, NPCID.TheDestroyer, NPCID.TheDestroyerBody, NPCID.TheDestroyerTail, NPCID.Probe, NPCID.PrimeCannon, NPCID.PrimeLaser, NPCID.PrimeSaw, NPCID.PrimeVice, NPCID.PlanterasHook, NPCID.PlanterasTentacle, NPCID.GolemFistLeft, NPCID.GolemFistRight, NPCID.GolemHead, NPCID.GolemHeadFree, NPCID.CultistTablet, NPCID.CultistBossClone, NPCID.LunarTowerNebula, NPCID.LunarTowerSolar, NPCID.LunarTowerVortex, NPCID.LunarTowerStardust, NPCID.MoonLordCore, NPCID.MoonLordHand, NPCID.MoonLordHead, NPCID.MoonLordFreeEye, NPCID.MoonLordLeechBlob };
-       public override void PostAI(NPC npc)
+        public override void SetDefaults(NPC npc)
         {
-
+            float randomSize = Main.rand.NextFloat(0.1f, 1.9f);
             if (npc.boss)
             {
-                
-                npc.scale = 1.9f;
+                npc.scale = randomSize;
             } else
             {
                 foreach (int bosspart in BossParts)
                 {
                     if (npc.type == bosspart)
                     {
-                        npc.scale = 1.9f;
+                        npc.scale = randomSize;
                     }
                 }
             }
-            base.PostAI(npc);
+            base.SetDefaults(npc);
         }
     }
 }
